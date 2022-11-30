71bd901347cd5d1fb230318a6acf1438ca6072d8
Adalberto Holguin <adalhs@yahoo.com>
2022-11-30 15:28:38 -0500

    Add Service Layer methods

--- FlooringMastery/src/main/java/com/sg/assessment/service/FlooringMasterServiceImpl.java
+++ FlooringMastery/src/main/java/com/sg/assessment/service/FlooringMasterServiceImpl.java
@@ -1,4 +1,33 @@
 package com.sg.assessment.service;
 
+import com.sg.assessment.model.State;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class FlooringMasterServiceImpl implements FlooringMasteryService {
+
+
+    @Override
+    public BigDecimal calculateMaterialCost(BigDecimal area, BigDecimal costPerSquareFoot) {
+        return area.multiply(costPerSquareFoot).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    @Override
+    public BigDecimal calculateLaborCost(BigDecimal area, BigDecimal laborCostPerSquareFoot) {
+        return area.multiply(laborCostPerSquareFoot).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    @Override
+    public BigDecimal calculateTax(BigDecimal materialCost, BigDecimal laborCost, State state) {
+        BigDecimal stateTax = state.getTaxRate().divide(new BigDecimal("100"), 15, RoundingMode.HALF_EVEN);
+        BigDecimal costPlusLabor = materialCost.add(laborCost);
+        return costPlusLabor.multiply(stateTax).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    @Override
+    public BigDecimal calculateTotal(BigDecimal materialCost, BigDecimal laborCost, BigDecimal tax) {
+        BigDecimal materialsPlusLabor = materialCost.add(laborCost);
+        return materialsPlusLabor.add(tax);
+    }
 }
