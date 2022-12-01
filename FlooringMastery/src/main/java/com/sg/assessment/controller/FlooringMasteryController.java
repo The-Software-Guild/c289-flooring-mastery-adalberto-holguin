115a180c92c68da481c226b5b750a85f307ff43a
prantik <prantik2000roy@gmail.com>
2022-12-01 11:16:29 -0500

    working on view

--- FlooringMastery/src/main/java/com/sg/assessment/controller/FlooringMasteryController.java
+++ FlooringMastery/src/main/java/com/sg/assessment/controller/FlooringMasteryController.java
@@ -27,15 +27,17 @@ public class FlooringMasteryController {
         boolean keepGoing = true;
         int menuSelection = 0;
         while (keepGoing) {
-            io.print("<<Flooring Program>>");
+            /*io.print("<<Flooring Program>>");
             io.print("1. Display Orders");
             io.print("2. Add an Order");
             io.print("3. Edit an Order");
             io.print("4. Remove an Order");
             io.print("5. Export All Data");
             io.print("6. Quit");
+*/          view.printMenu();
 
             menuSelection = io.readInt("Please select from the above choices.", 1, 6);
+
 
             switch (menuSelection) {
                 case 1:
@@ -63,9 +65,7 @@ public class FlooringMasteryController {
         }
         exitMessage();
     }
-    private int getMenuSelection() {
-        return view.printMenuAndGetSelection();
-    }
+    
     private void displayOrder() {
 
     }
