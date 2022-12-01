1f1478417415bd3491750cb65f161fe062f53aea
Selena <selenaxguillen@gmail.com>
2022-12-01 11:14:33 -0500

    commit dao change and new Order constructor

--- FlooringMastery/src/main/java/com/sg/assessment/dao/FlooringMasteryDao.java
+++ FlooringMastery/src/main/java/com/sg/assessment/dao/FlooringMasteryDao.java
@@ -2,65 +2,66 @@ package com.sg.assessment.dao;
 
 import com.sg.assessment.model.Order;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
 public interface FlooringMasteryDao {
+
     /**
      * Returns a List of all Orders regardless of status.
      *
-     * @return Order List containing all orders.
-     * @throws UnsupportedOperationException
+     * @return Order List containing all orders from current file.
+     * @throws UnsupportedOperationException replace later
      */
 
     List<Order> getAllOrders()
             throws UnsupportedOperationException;
 
-    /**
-     * Returns a List of all Orders based on date.
-     *
-     * @return Order List containing all orders based on date.
-     * @throws UnsupportedOperationException
-     */
-    List<Order> getOrdersByDate(LocalDate date)
-            throws UnsupportedOperationException;
 
     /**
-     * Adds an Order to the database and associates it with the given order number.
+     * Adds an Order to the file and associates it with the given order number.
      *
      * @param orderNumber order number to be associated with Order.
      * @param order order to be placed.
      *
-     * @throws UnsupportedOperationException
+     * @throws UnsupportedOperationException replace later
      */
 
-    Order addOrder(int orderNumber, Order order)
+    Order addOrder(LocalDate date, String customerName, String state, String productType, BigDecimal area)
             throws UnsupportedOperationException;
 
     /**
-     * Retrieves an Order from the database by its order number and returns it.
+     * Retrieves an Order from the file by its order number and returns it.
      * Returns null if no such order exists.
      *
      * @param orderNumber order number to be associated with Order.
-     * @param date date to be associated with the Order.
      *
-     * @throws UnsupportedOperationException
+     * @throws UnsupportedOperationException replace later
      */
 
-    Order getOrder(LocalDate date, int orderNumber)
+    Order getOrder(int orderNumber)
             throws UnsupportedOperationException;
 
     /**
-     * Removes from the roster the student associated with the given id.
-     * Returns the student object that is being removed or null if
-     * there is no student associated with the given id
+     * Removes the order associated with the order number.
+     * Returns null if no such order exists.
      *
-     * @param  date date of order to be removed
-     * @param orderNumber number of order to be removed
-     * @return Order object that was removed or null if no order
-     * was associated with the given order number
-     * @throws UnsupportedOperationException
+     * @param orderNumber order number to be associated with the order.
+     *
+     * @throws UnsupportedOperationException replace later
      */
-    Order removeOrder(LocalDate date, int orderNumber)
+    Order removeOrder(int orderNumber)
+            throws UnsupportedOperationException;
+
+    /**
+     * Edits properties for the order associated with the order number.
+     *
+     * @param orderNumber order number to be associated with the order.
+     *
+     * @throws UnsupportedOperationException replace later
+     */
+    void editOrder(int orderNumber, String newCustomerName, String newState,
+                   String newProductType, BigDecimal area)
             throws UnsupportedOperationException;
 }
